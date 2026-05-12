ae7e0b1b4f144071c0f7daaaa2a16ae1500ea5be
Jayden Sakow <jaydensakow@gmail.com>
2026-05-11 21:46:40 -0700

    Added enemy spawn

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -5,13 +5,8 @@ public static class GameParameters
 {
     public static float PlayerSpeed = 5f;
     public static float PlayerAttackCooldown = 1f;
+
+    public static float EnemyMinimumSpawnDelay = 1f;
+    public static float EnemyMaximumSpawnDelay = 3f;
     
-    public static Key MoveLeft  = Key.A;
-    public static Key MoveRight = Key.D;
-    public static Key MoveUp    = Key.W;
-    public static Key MoveDown  = Key.S;
-    public static Key AttackKey  = Key.Space;
-    
-    public static Key PlaceKey  = Key.E;
-    public static Key PickupKey = Key.F;
 }
